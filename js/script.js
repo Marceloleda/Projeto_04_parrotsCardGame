cd6eb5c138da586424623ccd43d7337020c47cf5
Marcelo Leda <marcelo.leda@icomp.ufam.edu.com.br>
2022-04-11 12:11:40 -0400

    feito o giro das cartas

--- js/script.js
+++ js/script.js
@@ -1,18 +1,52 @@
-let carta;
-
+//embaralha as cartas
 function comparador() { 
-	return Math.random() - 0.5; 
+    return Math.random() - 0.5; 
 }
+// let cartasParaBaixo = ["carta4"];
 
-let quantidade = prompt("Digite  com quantas cartas quer jogar: ");
-while(quantidade % 2 == 1 | quantidade > 14 | quantidade < 4){
-    quantidade = prompt("Digite somente números pares de 4 a 14: ");
+// cartasParaBaixo.sort(comparador);
+
+let carta;
+let numeroDeCartas;
+let contador =[];
+let imgCartasParaBaixo = ["./midia/front.png"];
+let imagemParrots = [
+    "./midia/bobrossparrot.gif",
+    "./midia/explodyparrot.gif",
+    "./midia/fiestaparrot.gif",
+    "./midia/metalparrot.gif",
+    "./midia/revertitparrot.gif",
+    "./midia/tripletsparrot.gif",
+    "./midia/unicornparrot.gif"];
+
+numeroDeCartas = prompt("Digite  com quantas cartas quer jogar: ");
+while(numeroDeCartas % 2 == 1 | numeroDeCartas > 14 | numeroDeCartas < 4){
+    numeroDeCartas = prompt("Digite somente números pares de 4 a 14: ");
 }
-let cartasParaBaixo = ["carta1","carta2","carta3","carta4"];
-cartasParaBaixo.sort(comparador);
-carta = document.querySelector(".cartas");
-for(let i = 0; i < quantidade; i++){
+for(let i = 0; i < numeroDeCartas; i++){
+    carta = document.querySelector(".cartas");
     carta.innerHTML +=`
-    <div class="carta">${cartasParaBaixo[i]}</div>`
+    <div class="carta" onclick = "virarCarta(this)">
+    <img src="${imgCartasParaBaixo[0]}"/>
+    </div>`;
+
 }
-//colocar as cartas em um array
+//colocar as cartas em um array(talvz)
+function virarCarta(elementoCarta) {
+    elementoCarta.style.transform = "rotateY(180deg)";
+    let img = elementoCarta.querySelector('img');
+    img.style.display = "none";
+    adicionarParrot(elementoCarta);
+}
+// function desviraCarta(){
+//     while(){
+//         const imgParrots = document.querySelector(".carta img");
+//         imgParrots.innerHTML +=`
+//         <img src="${imagemParrots[0]}"/>`;
+//     }
+// }
+function adicionarParrot(elementoCarta){
+    const addParrot = elementoCarta.querySelector("img");
+    addParrot.innerHTML += `
+    <img src="${imagemParrots[1]}">`;
+}
